30fb685174abb3fabf3a02cae36c9e4d3e9532fe
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-05-26 23:37:30 +0500

    Register database services at startup

--- Blazor Task Management System/Program.cs
+++ Blazor Task Management System/Program.cs
@@ -1,4 +1,8 @@
 using Blazor_Task_Management_System.Components;
+using Blazor_Task_Management_System.Data;
+using Blazor_Task_Management_System.Services;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +10,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var configuredConnectionString = builder.Configuration.GetConnectionString("TodoDatabase")
+    ?? "Data Source=todo_tasks.db";
+var connectionBuilder = new SqliteConnectionStringBuilder(configuredConnectionString);
+
+if (!Path.IsPathRooted(connectionBuilder.DataSource))
+{
+    connectionBuilder.DataSource = Path.Combine(
+        builder.Environment.ContentRootPath,
+        connectionBuilder.DataSource);
+}
+
+var databaseInfo = new DatabaseInfo(connectionBuilder.DataSource, connectionBuilder.ToString());
+
+builder.Services.AddSingleton(databaseInfo);
+builder.Services.AddDbContextFactory<TodoDbContext>(options =>
+    options.UseSqlite(databaseInfo.ConnectionString));
+builder.Services.AddScoped<TodoTaskService>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await TodoDatabaseInitializer.InitializeAsync(scope.ServiceProvider);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
